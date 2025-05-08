4624b2fae32d0640298fda9a7b0d03b98d91682d
ralm <ralm@ralms-MacBook-Air.local>
2025-05-08 16:42:03 +0300

    Authentication service successfuly  developed

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,7 @@
+import 'package:barber_booking_app/pages/home.dart';
 import 'package:barber_booking_app/pages/signup.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -9,6 +12,62 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  String? userEmail, userPassword;
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  final _formkey =
+      GlobalKey<FormState>(); //Form key to validate the form (Error Handling)
+
+  userLogin() async {
+    try {
+      // Check the password passed is correct
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text,
+        password:
+            passwordController
+                .text, //Consider using a trim method to remove any leading or trailing spaces
+      );
+      Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+              'No user found for that email.',
+              style: TextStyle(fontSize: 20),
+            ),
+            backgroundColor: Colors.red,
+            duration: Duration(seconds: 2),
+          ),
+        );
+      } else if (e.code == 'wrong-password') {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+              'Wrong password provided for that user.',
+              style: TextStyle(fontSize: 20),
+            ),
+            backgroundColor: Colors.red,
+            duration: Duration(seconds: 2),
+          ),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+              'Error occurred while signing in.',
+              style: TextStyle(fontSize: 20),
+            ),
+            backgroundColor: Colors.red,
+            duration: Duration(seconds: 2),
+          ),
+        );
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,104 +118,132 @@ class _LoginState extends State<Login> {
                   topRight: Radius.circular(40),
                 ),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Email:',
-                    style: TextStyle(
-                      color: Color(0xFFB91635),
-                      fontSize: 25.0,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'aaaaaaa@yourmail.com',
-                      prefixIcon: Icon(Icons.mail_outlined),
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    'Password:',
-                    style: TextStyle(
-                      color: Color(0xFFB91635),
-                      fontSize: 25.0,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Write a strong password',
-                      prefixIcon: Icon(Icons.password),
-                    ),
-                    obscureText: true,
-                  ),
-                  SizedBox(height: 15),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        'Forget Password',
-                        style: TextStyle(
-                          color: Color(0xFF621d3c),
-                          fontSize: 15.0,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 40),
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 10),
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [
-                          Color(0xFFB91635),
-                          Color(0xFF621d3c),
-                          Color(0xFF311937),
-                        ],
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                      ),
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'Sign In',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 26.0,
-                          fontWeight: FontWeight.bold,
-                        ),
+              child: Form(
+                key: _formkey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Email:',
+                      style: TextStyle(
+                        color: Color(0xFFB91635),
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.w500,
                       ),
                     ),
-                  ),
-                  SizedBox(height: 5),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => Signup()),
-                          );
-                        },
-                        child: Text(
-                          'Don\'t have an account? \nSign Up',
-                          textAlign: TextAlign.end,
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your email';
+                        }
+                        return null;
+                      },
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        hintText: 'JohnDoe@mymail.com',
+                        prefixIcon: Icon(Icons.mail_outlined),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      'Password:',
+                      style: TextStyle(
+                        color: Color(0xFFB91635),
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your password';
+                        }
+                        return null;
+                      },
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        hintText: 'Write a strong password',
+                        prefixIcon: Icon(Icons.password),
+                      ),
+                      obscureText: true,
+                    ),
+                    SizedBox(height: 15),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Forget Password',
                           style: TextStyle(
                             color: Color(0xFF621d3c),
                             fontSize: 15.0,
                             fontWeight: FontWeight.w500,
                           ),
                         ),
+                      ],
+                    ),
+                    SizedBox(height: 40),
+                    GestureDetector(
+                      onTap: () {
+                        if (_formkey.currentState!.validate()) {
+                          setState(() {
+                            userEmail = emailController.text;
+                            userPassword = passwordController.text;
+                          });
+                          userLogin();
+                        }
+                      },
+                      child: Container(
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Color(0xFFB91635),
+                              Color(0xFF621d3c),
+                              Color(0xFF311937),
+                            ],
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                          ),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Sign In',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 26.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
                       ),
-                    ],
-                  ),
-                ],
+                    ),
+                    SizedBox(height: 5),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => Signup()),
+                            );
+                          },
+                          child: Text(
+                            'Don\'t have an account? \nSign Up',
+                            textAlign: TextAlign.end,
+                            style: TextStyle(
+                              color: Color(0xFF621d3c),
+                              fontSize: 15.0,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -1,4 +1,6 @@
+import 'package:barber_booking_app/pages/home.dart';
 import 'package:barber_booking_app/pages/login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Signup extends StatefulWidget {
@@ -9,6 +11,62 @@ class Signup extends StatefulWidget {
 }
 
 class _SignupState extends State<Signup> {
+  String? userName, userEmail, userPassword;
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  final _formkey =
+      GlobalKey<FormState>(); //Form key to validate the form (Error Handling)
+
+  registration() async {
+    if (userPassword != null && userName != null && userEmail != null) {
+      try {
+        UserCredential userCredential = await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(
+              email: userEmail!,
+              password: userPassword!,
+            );
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+              'Registered Successfully',
+              style: TextStyle(fontSize: 20),
+            ),
+            backgroundColor: Colors.green,
+          ),
+        );
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Home()),
+        );
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'weak-password') {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                'The password provided is too weak.',
+                style: TextStyle(fontSize: 20),
+              ),
+              backgroundColor: Colors.red,
+            ),
+          );
+        } else if (e.code == 'email-already-in-use') {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                'Account already exists for that email.',
+                style: TextStyle(fontSize: 20),
+              ),
+              backgroundColor: Colors.red,
+            ),
+          );
+        }
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,119 +117,158 @@ class _SignupState extends State<Signup> {
                   topRight: Radius.circular(40),
                 ),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Name:',
-                    style: TextStyle(
-                      color: Color(0xFFB91635),
-                      fontSize: 25.0,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'John Doe',
-                      prefixIcon: Icon(Icons.mail_outlined),
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    'Email:',
-                    style: TextStyle(
-                      color: Color(0xFFB91635),
-                      fontSize: 25.0,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'JohnDoe@mymail.com',
-                      prefixIcon: Icon(Icons.mail_outlined),
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    'Password:',
-                    style: TextStyle(
-                      color: Color(0xFFB91635),
-                      fontSize: 25.0,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Write a strong password',
-                      prefixIcon: Icon(Icons.password),
-                    ),
-                    obscureText: true,
-                  ),
-                  SizedBox(height: 15),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        'Forget Password',
-                        style: TextStyle(
-                          color: Color(0xFF621d3c),
-                          fontSize: 15.0,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 40),
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 10),
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [
-                          Color(0xFFB91635),
-                          Color(0xFF621d3c),
-                          Color(0xFF311937),
-                        ],
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                      ),
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'Sign Up',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 26.0,
-                          fontWeight: FontWeight.bold,
-                        ),
+              child: Form(
+                key: _formkey, //Form key to validate the form (Error Handling)
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Name:',
+                      style: TextStyle(
+                        color: Color(0xFFB91635),
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.w500,
                       ),
                     ),
-                  ),
-                  SizedBox(height: 5),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => Login()),
-                          );
-                        },
-                        child: Text(
-                          'Already have an account? \nSign In',
-                          textAlign: TextAlign.end,
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your name';
+                        }
+                        return null;
+                      },
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        hintText: 'John Doe',
+                        prefixIcon: Icon(Icons.mail_outlined),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      'Email:',
+                      style: TextStyle(
+                        color: Color(0xFFB91635),
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your email';
+                        }
+                        return null;
+                      },
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        hintText: 'JohnDoe@mymail.com',
+                        prefixIcon: Icon(Icons.mail_outlined),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      'Password:',
+                      style: TextStyle(
+                        color: Color(0xFFB91635),
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your password';
+                        }
+                        return null;
+                      },
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        hintText: 'Write a strong password',
+                        prefixIcon: Icon(Icons.password),
+                      ),
+                      obscureText: true,
+                    ),
+                    SizedBox(height: 15),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Forget Password',
                           style: TextStyle(
                             color: Color(0xFF621d3c),
                             fontSize: 15.0,
                             fontWeight: FontWeight.w500,
                           ),
                         ),
+                      ],
+                    ),
+                    SizedBox(height: 40),
+                    GestureDetector(
+                      onTap:
+                          () => {
+                            if (_formkey.currentState!.validate())
+                              {
+                                setState(() {
+                                  // To store the values in the variables
+                                  userName = nameController.text;
+                                  userEmail = emailController.text;
+                                  userPassword = passwordController.text;
+                                }),
+                              },
+                            registration(), // Call the registration function
+                          },
+                      child: Container(
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Color(0xFFB91635),
+                              Color(0xFF621d3c),
+                              Color(0xFF311937),
+                            ],
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                          ),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Sign Up',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 26.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
                       ),
-                    ],
-                  ),
-                ],
+                    ),
+                    SizedBox(height: 5),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => Login()),
+                            );
+                          },
+                          child: Text(
+                            'Already have an account? \nSign In',
+                            textAlign: TextAlign.end,
+                            style: TextStyle(
+                              color: Color(0xFF621d3c),
+                              fontSize: 15.0,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
