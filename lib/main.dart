8363a3af18e2582981fa15b712575f1a023429b5
ralm <ralm@ralms-MacBook-Air.local>
2025-05-12 13:27:15 +0300

    Added new rules for mainting the code and following the guidlines

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: ForgetPassword(),
+      home: Signup(),
     );
   }
 }
